36154457006dadcfe75414f7e65a831b28e1a4e8
Sarah Wolff <sarah@serabi.dev>
2026-04-02 13:02:48 -0400

    refactor: deduplicate relativeTimeUntil past-branch by delegating to relativeTime

--- model_detail.go
+++ model_detail.go
@@ -353,19 +353,7 @@ func relativeTimeUntil(iso string) string {
 	}
 	d := time.Until(t)
 	if d < 0 {
-		d = -d
-		switch {
-		case d < time.Minute:
-			return "just now"
-		case d < time.Hour:
-			return fmt.Sprintf("%dm ago", int(d.Minutes()))
-		case d < 24*time.Hour:
-			return fmt.Sprintf("%dh ago", int(d.Hours()))
-		case d < 30*24*time.Hour:
-			return fmt.Sprintf("%dd ago", int(d.Hours()/24))
-		default:
-			return t.Format("Jan 2")
-		}
+		return relativeTime(iso)
 	}
 	switch {
 	case d < time.Minute:
